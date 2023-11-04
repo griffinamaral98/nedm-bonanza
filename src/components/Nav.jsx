67503166916a2dd1ae9bcec66c9d638712f1c23c
Griffin Amaral <griffin.amaral98@gmail.com>
2023-11-04 16:01:07 -0400

    fixed text color

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -69,7 +69,7 @@ const Nav = () => {
                 id="dropdownNavbarLink"
                 data-dropdown-toggle="dropdownNavbar"
                 data-dropdown-placement="right-start"
-                className="flex items-center justify-between w-full py-2 pl-3 pr-4  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                className="flex items-center justify-between w-full py-2 pl-3 pr-4   border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                 onClick={() => setDropdownIsOpen(!dropDownIsOpen)}
               >
                 Applications

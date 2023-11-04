7f1b63edfe63ab3938b1b7241b785238d5e2e6e8
Griffin Amaral <griffin.amaral98@gmail.com>
2023-11-03 22:43:05 -0400

    Updated instagram link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
           <a
-            href="#"
+            href="https://www.instagram.com/nedm_bonanza/"
             className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             <svg
